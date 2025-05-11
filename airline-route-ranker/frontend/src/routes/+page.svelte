2868013a32db7ebf9ffcd8022fa324b8c97a7bd4
Asad-Ismail <asadismaeel@gmail.com>
2025-05-11 17:56:27 +0200

    updated banner

--- airline-route-ranker/frontend/src/routes/+page.svelte
+++ airline-route-ranker/frontend/src/routes/+page.svelte
@@ -657,7 +657,7 @@
             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2 text-yellow-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            <span class="font-bold text-sm md:text-base">Sign up for FREE and get 2 search credits! Additional credits just $1.99 for 5.</span>
+            <span class="font-bold text-sm md:text-base">Sign up for FREE and get 2 search credits! Additional 5 credits for just $1.99</span>
           </div>
           <a href="/signup" class="bg-white hover:bg-sky-50 text-sky-dark font-bold py-2 px-6 rounded-full transition-colors shadow-md">
             Create Account
